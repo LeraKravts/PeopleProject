4624d96a933e01455a9d49e48f3a246f426121e6
ValeriaKravtsova <lera.gor@inbox.ru>
2021-06-14 21:33:06 +0300

    Initial Commit

---
+++ PeopleProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PeopleProject
+//
+//  Created by Valeriya on 14.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
